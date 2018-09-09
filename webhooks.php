b98e7096961439162811875d22c0bad5a0cde7b8
unqthn <yong161@gmail.com>
2018-09-09 21:43:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -37,7 +37,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages]
 				
 			];
 			$post = json_encode($data);
